ac26c55547104c30c66a10fbedac5dd704f0ce02
Manfred <mtan00@stuy.edu>
2020-05-09 21:20:59 -0400

    added representative page

--- test/app.py
+++ test/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, redirect, session, render_template, url_for, flash
+from urllib.request import urlopen
+import json
 import os
 
 app = Flask(__name__)
@@ -8,6 +10,33 @@ app.secret_key = os.urandom(32)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/help", methods=['GET', 'POST'])
+def help():
+
+    #req = request.form['zip', False]
+    #link = urlopen("https://whoismyrepresentative.com/getall_mems.php?output=json&zip=11357")
+    #response = link.read()
+    #data = json.loads( response )
+    #print(data)
+    #results = data['results']
+    return render_template("help.html", results=[])
+
+@app.route("/zip", methods=['GET', 'POST'])
+def zip():
+    zip = request.form['zip']
+    print(zip)
+    link = "https://whoismyrepresentative.com/getall_mems.php?output=json&zip=" + zip
+    try:
+        open = urlopen(link)
+        response = open.read()
+        data = json.loads( response )
+        results = data['results']
+    except:
+        results = [{'name': 'Invalid ZIP code', 'link':''}]
+    return render_template("help.html", results=results)
+
 
 if __name__ == "__main__":
     app.debug = True
